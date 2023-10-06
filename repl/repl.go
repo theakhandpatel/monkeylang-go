80c4a7b172ad0318388824786d9b397560c82be0
akhand <akhand3108patel@gmail.com>
2023-10-06 21:42:56 +0530

    feat: add parsing in repl

--- repl/repl.go
+++ repl/repl.go
@@ -7,8 +7,10 @@ import (
 	"io"
 
 	"github.com/theakhandpatel/interpreter/lexer"
-	"github.com/theakhandpatel/interpreter/token"
+	"github.com/theakhandpatel/interpreter/parser"
 )
+
+const ANGRY_FACE = `【≽ܫ≼】`
 
 const PROMPT = ">> "
 
@@ -24,9 +26,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, ANGRY_FACE)
+	io.WriteString(out, "Woops! We ran into some funky business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
