61479ad3723fb4c903fe2b8303b4ded02a6a8b9a
akhand <akhand3108patel@gmail.com>
2023-10-08 13:26:12 +0530

    FEAT: Add Evaluating Expressions

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 
 	"io"
 
+	"github.com/theakhandpatel/interpreter/evaluator"
 	"github.com/theakhandpatel/interpreter/lexer"
 	"github.com/theakhandpatel/interpreter/parser"
 )
@@ -34,8 +35,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
